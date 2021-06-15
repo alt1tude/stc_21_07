c9b47a6a5f1ba1eda19f88baeed36839787c35da
Artem <fort1tude@mail.ru>
2021-06-16 04:38:38 +0700

    rework task2_3

--- Task_2/array_to_number/src/Main.java
+++ Task_2/array_to_number/src/Main.java
@@ -2,17 +2,19 @@ import java.util.Arrays;
 public class Main {
     // работает, только для чисел из разряд единиц и десяток
     public static void main(String[] args) {
-        int[] array = {11, 91};
+        int[] array = {10, 11, 6, 10, 7};
         System.out.print("Array: ");
         System.out.println(Arrays.toString(array));
         int number = 0;
-        for (int i : array) {
-            if (i < 10) {
-                number = number * 10;
-            } else {
-                number = number * 100;
+        int base;
+        for (int i = 0; i < array.length; ++i) {
+            if (array[i] < 10) {
+                base = 10;
+                number = number * base + array[i];
+            } else if (array[i] < 100) {
+                base = 100;
+                number = number * base + array[i];
             }
-            number = number + i;
         }
         System.out.println("This array as a number: " + number);
     }
